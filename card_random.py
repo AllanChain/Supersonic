ecbe69846dc06563fd0080cb0e5af4f3c46b6197
AllanChain <txsmlf@gmail.com>
2018-08-24 08:13:41 +0800

    completed comparation

--- card_random.py
+++ card_random.py
@@ -3,3 +3,12 @@ _cards=list(range(1,13+1))
 shuffle(_cards)
 def peer(index):
     return _cards[index]
+def conservative():
+    step=25
+    x=1
+    while _cards.index(x+1)>_cards.index(x):
+        step-=1
+        x+=1
+    return step
+def rerand():
+    shuffle(_cards)
--- compare.py
+++ compare.py
@@ -1,5 +1,4 @@
-from card_random import peer
-import colors
+from card_random import peer,conservative,rerand
 
 def main():
     operation=0
@@ -18,5 +17,14 @@ def main():
                 operation+=1
                 max_expose+=1
                 exposed[known[max_expose]]=max_expose
+    return operation
 if __name__=='__main__':
-    main()
+    total=0
+    for i in range(100):
+        operation=main()
+        c_operation=conservative()
+        delta=c_operation-operation
+        total+=delta
+        print('conservative {},radical {},delta {}'.format(c_operation,operation,delta))
+        rerand()
+    print('average {}'.format(total/(i+1)))
